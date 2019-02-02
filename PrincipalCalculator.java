75eb986791d90da55348da0cf34b21357c171558
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-02-01 21:21:02 -0600

    Add files via upload

--- PrincipalCalculator.java
+++ PrincipalCalculator.java
@@ -12,33 +12,32 @@ import java.io.IOException;
  * <p>Clase principal del programa, lee un archivo de texto en notacion postfix y realiza las operaciones  </p>
  * */
 public class PrincipalCalculator {
-   
- public static void main(String[] args) throws FileNotFoundException, IOException 
+
+    public static void main(String[] args) throws FileNotFoundException, IOException
     {
-       
         Pila doc = new Pila();
         Calcu nueva = new Calcu();
-    //Se declaran las variables que seran utiles para leer el archivo de tipo texto
-     
-    FileReader fr = new FileReader("datos.txt"); //aqui debes de poner tu ruta
-     BufferedReader br = new BufferedReader (fr);
+        //Se declaran las variables que seran utiles para leer el archivo de tipo texto
 
-   
-     String linea;
-     while((linea=br.readLine())!=null) 
+        FileReader fr = new FileReader("datos.txt"); //aqui debes de poner tu ruta
+        BufferedReader br = new BufferedReader (fr);
+
+
+        String linea;
+        while((linea=br.readLine())!=null)
         {
-            
+
             //caract guarda el numero de caracter, por lo que lo casteamos a char para que nos de
             //el caracter como lo necesitamos
             //System.out.println((char)caract);
             String cadena=linea;
-            
+
             String[] post;
             post = cadena.split(" ");
             int largo= post.length;
-            
-              for(int i=0; i< largo; i++)
-              {
+
+            for(int i=0; i< largo; i++)
+            {
                 if ((!post[i].equals("+"))&& (!post[i].equals("-")) && (!post[i].equals("*")) && (!post[i].equals("/")) ){
                     int numero= Integer.parseInt (post[i]);
                     doc.push(numero);
@@ -50,12 +49,15 @@ public class PrincipalCalculator {
                     int n2= (int) doc.pop();
                     int r= nueva.Calculate(n1, n2,operando);
                     System.out.println("El resultado de la operacion de "+n2+ " "+ operando+ " "+n1+ " es: "+r);
-               
-                    
-             }
-           
-           }
+
+
+
+                }
+
+            }
+
         }
     }
- 
-    }
+}
+
+
