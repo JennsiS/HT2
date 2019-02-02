0e34245902a0c2aced1133308966a9d11c2b9b96
Jenn <san18962@uvg.edu.gt>
2019-02-01 20:57:18 -0600

    Update PrincipalCalculator.java
    Ultimo cambio a la clase principal

--- PrincipalCalculator.java
+++ PrincipalCalculator.java
@@ -1,7 +1,5 @@
 
-import java.util.Vector;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,32 +18,46 @@ public class PrincipalCalculator {
         Pila doc = new Pila();
         Calcu nueva = new Calcu();
     //Se declaran las variables que seran utiles para leer el archivo de tipo texto
-     FileReader fr = new FileReader("datos.txt"); //aqui debes de poner tu ruta
-     int caract = fr.read();
      
-     while(caract != -1) 
+    FileReader fr = new FileReader("datos.txt"); //aqui debes de poner tu ruta
+     BufferedReader br = new BufferedReader (fr);
+
+   
+     String linea;
+     while((linea=br.readLine())!=null) 
         {
-            caract = fr.read();
+            
             //caract guarda el numero de caracter, por lo que lo casteamos a char para que nos de
             //el caracter como lo necesitamos
-            System.out.println((char)caract);
-            String cadena= Integer.toString (caract);
+            //System.out.println((char)caract);
+            String cadena=linea;
+            
             String[] post;
             post = cadena.split(" ");
-            for (int i=0; i<post.length; i++){
-                while ((!post.equals("+"))&& (!post.equals("-")) && (!post.equals("*")) && (!post.equals("/")) ){
+            int largo= post.length;
+            
+              for(int i=0; i< largo; i++)
+              {
+                if ((!post[i].equals("+"))&& (!post[i].equals("-")) && (!post[i].equals("*")) && (!post[i].equals("/")) ){
                     int numero= Integer.parseInt (post[i]);
                     doc.push(numero);
+                    System.out.println();
                 }
-            if (post[i].equals("+-*/%")){
+                else if ((post[i].equals("+")) || (post[i].equals("-")) || (post[i].equals("*")) || (post[i].equals("/")) ){
+                    String operando= post[i];
                     int n1= (int) doc.pop();
                     int n2= (int) doc.pop();
-                    int r=nueva.Calculate(n1, n2, post[i]);
-                    System.out.println(r);
+                    int r= nueva.Calculate(n1, n2,operando);
+                    System.out.println("El resultado de la operacion de "+n2+ " "+ operando+ " "+n1+ " es: "+r);
+                 
+                 
+                            
+                }
+                    
              }
            
            }
         }
     }
- }
+ 
     
